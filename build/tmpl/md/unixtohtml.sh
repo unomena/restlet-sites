c7ebe0d7c41de5285daead2cac61798da3e985ff
Thierry Boileau <tboileau@restlet.com>
2013-01-30 19:28:38 +0100

    dded

---
+++ build/tmpl/md/unixtohtml.sh
@@ -0,0 +1,3 @@
+find $1 -name '*.md' -exec pandoc -f markdown -t html -s --template=$2 --base-header-level=4 -o '{}.html' '{}' \;
+find $1 -name '*.md' -delete
+find $1 -name '*.md.html' -exec rename s/\.md.html$/\.html/ {} \;
