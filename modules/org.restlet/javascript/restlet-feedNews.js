0ac6cca763b96f35af7d3e6730fb1a325fad6d24
Thierry Boileau <tboileau@restlet.com>
2013-04-05 09:43:39 +0200

    Fixed news pages, fixed code blocks in user guide.

--- modules/org.restlet/javascript/restlet-feedNews.js
+++ modules/org.restlet/javascript/restlet-feedNews.js
@@ -6,7 +6,7 @@
 			var listContent = "";
    			var elements = jsonRepresentation.getObject();
    			if(elements.length && elements.length>0){
-	   			listContent = "<h4 class=\"blog\"><a href=\"/http://blog.restlet.com/feed/?cat=894539\">Restlet - General&nbsp;<img src=\"/images/icons/16/feed-icon\" alt=\"Syndication feed\" style=\"margin:5px\"/></a></h4>";
+	   			listContent = "<h4 class=\"blog\"><a href=\"http://blog.restlet.com/feed/?cat=894539\">Restlet - General&nbsp;<img src=\"/images/icons/16/feed-icon\" alt=\"Syndication feed\" style=\"margin:5px\"/></a></h4>";
 	   			listContent += "<ul class=\"blog\">";
 	   			for (var i=0; i < elements.length; i++) {
 	   				listContent += createFeedElement(elements[i]);
@@ -22,7 +22,7 @@
 			var listContent = "";
    			var elements = jsonRepresentation.getObject();
    			if(elements.length && elements.length>0){
-	   			listContent = "<h4 class=\"blog\"><a href=\"/http://blog.restlet.com/feed/?cat=894542\">Restlet - Releases&nbsp;<img src=\"/images/icons/16/feed-icon\" alt=\"Syndication feed\" style=\"margin:5px\"/></a></h4>";
+	   			listContent = "<h4 class=\"blog\"><a href=\"http://blog.restlet.com/feed/?cat=894542\">Restlet - Releases&nbsp;<img src=\"/images/icons/16/feed-icon\" alt=\"Syndication feed\" style=\"margin:5px\"/></a></h4>";
 	   			listContent += "<ul class=\"blog\">";
 	   			for (var i=0; i < elements.length; i++) {
 	   				listContent += createFeedElement(elements[i]);
