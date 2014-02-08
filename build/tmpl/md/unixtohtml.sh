c55e453c7f077af3c1625cc7239e2c1b34d079ff
Thierry Boileau <tboileau@restlet.com>
2014-02-08 08:54:35 +0100

    Remove .md substrings.

--- build/tmpl/md/unixtohtml.sh
+++ build/tmpl/md/unixtohtml.sh
@@ -1,3 +1,4 @@
+find $1 -name '*.md' -exec sed -i 's#\.md##' {} \;
 find $1 -name '*.md' -exec pandoc -f markdown -t html -s --template=$2 --base-header-level=4 -o '{}.html' '{}' \;
 find $1 -name '*.md' -delete
 find $1 -name '*.md.html' -exec rename s/\.md.html$/\.html/ {} \;
