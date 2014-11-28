439d90120ce4404775c6b211952b80294940471b
thboileau <thboileau@gmail.com>
2014-11-28 10:10:47 +0100

    Fixed maven issue.

--- modules/com.restlet.frontend/src/com/restlet/frontend/web/applications/MavenRestletOrg.java
+++ modules/com.restlet.frontend/src/com/restlet/frontend/web/applications/MavenRestletOrg.java
@@ -4,10 +4,18 @@
 
 package com.restlet.frontend.web.applications;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.restlet.Client;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.Restlet;
+import org.restlet.data.LocalReference;
 import org.restlet.data.MediaType;
+import org.restlet.engine.local.Entity;
+import org.restlet.engine.local.FileClientHelper;
+import org.restlet.engine.local.FileEntity;
 import org.restlet.resource.Directory;
 import org.restlet.routing.Router;
 
@@ -18,49 +26,79 @@ import org.restlet.routing.Router;
  * @author Thierry Boileau
  */
 public class MavenRestletOrg extends BaseApplication {
-    /** The data file URI. */
-    private String dataUri;
+	/** The data file URI. */
+	private String dataUri;
 
-    /**
-     * Constructor.
-     * 
-     * @param propertiesFileReference
-     *            The Reference to the application's properties file.
-     * @throws IOException
-     */
-    public MavenRestletOrg(String propertiesFileReference) throws IOException {
-        super(propertiesFileReference);
+	/**
+	 * File client helper that does not try to infer file according to the
+	 * metadata.
+	 * 
+	 * @author thboileau
+	 * 
+	 */
+	private static class StrictFileClientHelper extends FileClientHelper {
+		public StrictFileClientHelper(Client client) {
+			super(client);
+		}
 
-        this.dataUri = getProperty("data.uri");
+		@Override
+		public Entity getEntity(String decodedPath) {
+			File file = new File(LocalReference.localizePath(decodedPath));
+			if (file.exists()) {
+				return new FileEntity(file, getMetadataService());
+			}
+			return null;
+		}
+	}
 
-        MediaType mediaType = new MediaType("application/sha1",
-                "Secured hash algorithm");
-        getMetadataService().addExtension("sha1", mediaType);
-        mediaType = new MediaType("application/md5", "Message-digest algorithm");
-        getMetadataService().addExtension("MD5", mediaType);
-        getMetadataService().addExtension("md5", mediaType);
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param propertiesFileReference
+	 *            The Reference to the application's properties file.
+	 * @throws IOException
+	 */
+	public MavenRestletOrg(String propertiesFileReference) throws IOException {
+		super(propertiesFileReference);
 
-    @Override
-    public Restlet createInboundRoot() {
-        // Create a root router
-        Router result = new Router(getContext());
+		this.dataUri = getProperty("data.uri");
 
-        // Serve repository
-        Directory directory = new Directory(getContext(), this.dataUri
-                + "/maven2/restlet");
-        directory.setIndexName(null);
-        directory.setNegotiatingContent(true);
-        directory.setModifiable(false);
-        directory.setDeeplyAccessible(true);
-        directory.setListingAllowed(true);
-        result.attachDefault(directory);
+		MediaType mediaType = new MediaType("application/sha1",
+				"Secured hash algorithm");
+		getMetadataService().addExtension("sha1", mediaType);
+		mediaType = new MediaType("application/md5", "Message-digest algorithm");
+		getMetadataService().addExtension("MD5", mediaType);
+		getMetadataService().addExtension("md5", mediaType);
+	}
 
-        return result;
-    }
+	@Override
+	public Restlet createInboundRoot() {
+		// Create a root router
+		Router result = new Router(getContext());
+		final FileClientHelper fch = new StrictFileClientHelper(null);
+		Restlet r = new Restlet(getContext()) {
+			@Override
+			public void handle(Request request, Response response) {
+				fch.handle(request, response);
+			}
+		};
+		getContext().setClientDispatcher(r);
 
-    @Override
-    public String getName() {
-        return "Application for maven.restlet.org";
-    }
+		// Serve repository
+		Directory directory = new Directory(getContext(), this.dataUri
+				+ "/maven2/restlet");
+		directory.setIndexName(null);
+		directory.setNegotiatingContent(true);
+		directory.setModifiable(false);
+		directory.setDeeplyAccessible(true);
+		directory.setListingAllowed(true);
+		result.attachDefault(directory);
+
+		return result;
+	}
+
+	@Override
+	public String getName() {
+		return "Application for maven.restlet.org";
+	}
 }
